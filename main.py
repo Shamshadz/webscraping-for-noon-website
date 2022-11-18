d941afa213a89456cfc77893ffeb32eb818ad3b8
Shamshadz <chaudhary.s.shamshad07@gmail.com>
2022-11-18 16:17:32 +0530

    update

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ source = requests.get('https://www.noon.com/egypt-en/sports-and-outdoors/exercis
 soup = BeautifulSoup(source, 'lxml')
 
 print(soup.prettify())
+
+# https://www.linkedin.com/jobs/search/?currentJobId=3288885160&geoId=100811329&keywords=Airline%20Pilot&location=Karnataka%2C%20India&refresh=true
+# https://www.linkedin.com/jobs/search/?currentJobId=3356060708&geoId=106164952&keywords=Airline%20Pilot&location=Mumbai%2C%20Maharashtra%2C%20India&refresh=true
+# https://www.linkedin.com/jobs/search/?currentJobId=3350527633&geoId=106187582&keywords=Airline%20Pilot&location=Delhi%2C%20India&refresh=true
+
+
+# https://www.linkedin.com/jobs/search/?currentJobId=3288885160&keywords=Airline%20Pilot&location=Karnataka%2C%20India&refresh=true
+# https://www.linkedin.com/jobs/search/?currentJobId=3356060708&keywords=Airline%20Pilot&location=Mumbai%2C%20Maharashtra%2C%20India&refresh=true
+# https://www.linkedin.com/jobs/search/?currentJobId=3350527633&keywords=Airline%20Pilot&location=Delhi%2C%20India&refresh=true
